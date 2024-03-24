3aa8259a08417b739fd7b60d8a7d97647929518b
AmnaBibiSaeedAhmed <160926756+AmnaBibiSaeedAhmed@users.noreply.github.com>
2024-03-24 15:32:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var weightInKg = 51;
+var heightInMeters = 1.58;
+var bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log("Your BMI ".concat(bmi));
---
+++ index.ts
@@ -0,0 +1,4 @@
+let weightInKg = 51;
+let heightInMeters = 1.58;
+let bmi = weightInKg / (heightInMeters * heightInMeters )
+console.log(`Your BMI ${bmi}`);
